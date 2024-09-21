f0c2aa9511e3c01c3b80d98515da287fd64f0960
yan-karlo <yan.karlo29@gmail.com>
2024-09-21 16:10:10 -0300

    infra: Makes infrastructure adjustments due to composer introduction

--- backend/public/src/App/bootstrap.php
+++ backend/public/src/App/bootstrap.php
@@ -1,11 +1,8 @@
 <?php
 
 declare(strict_types=1);
-
-include __DIR__."/../Framework/App.php";
-
+require __DIR__ . "/../../vendor/autoload.php";
 use Framework\App;
-
 $app = new App();
 
 return $app;
